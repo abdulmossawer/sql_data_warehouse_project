6a2812c6005a2e7dea95c3d0a1fceeae4d0c391a
Abdul Mossawer <142085179+abdulmossawer@users.noreply.github.com>
2025-08-04 20:48:06 +0530

    Update

--- scripts/silver/Quality_Check.sql
+++ scripts/silver/Quality_Check.sql
@@ -4,6 +4,49 @@
 SELECT
 	cst_id,
 	COUNT(*)
-FROM bronze.crm_cust_info
+FROM silver.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL
+
+
+-- Check for unwanted Spaces for cst_firstname
+-- Expectation: No Result
+
+SELECT
+	cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname ! = TRIM(cst_firstname) 
+
+
+-- Check for unwanted Spaces for cst_lastname
+-- Expectation: No Result
+
+SELECT 
+	cst_lastname
+FROM silver.crm_cust_info
+WHERE cst_lastname ! = TRIM(cst_lastname)
+
+
+-- Check for unwanted Spaces for gndr
+-- Expectation: No Result
+
+SELECT 
+	cst_gndr
+FROM silver.crm_cust_info
+WHERE cst_gndr ! = TRIM(cst_gndr)
+
+
+SELECT 
+	cst_key
+FROM silver.crm_cust_info
+WHERE cst_key ! = TRIM(cst_key)
+
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info
+
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info
+
+SELECT * FROM silver.crm_cust_info
