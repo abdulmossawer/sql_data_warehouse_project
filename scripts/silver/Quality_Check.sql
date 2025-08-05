6bbe1615ccc3b5fe175da5e1a22ecfab547a82f7
Abdul Mossawer <142085179+abdulmossawer@users.noreply.github.com>
2025-08-05 21:21:58 +0530

    Inserted silver.crm_prd_info

--- scripts/silver/Quality_Check.sql
+++ scripts/silver/Quality_Check.sql
@@ -2,51 +2,39 @@
 -- Expectation: No Result
 
 SELECT
-	cst_id,
+	prd_id,
 	COUNT(*)
-FROM silver.crm_cust_info
-GROUP BY cst_id
-HAVING COUNT(*) > 1 OR cst_id IS NULL
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
 
 
--- Check for unwanted Spaces for cst_firstname
+-- Check for unwanted Spaces 
 -- Expectation: No Result
 
 SELECT
-	cst_firstname
-FROM silver.crm_cust_info
-WHERE cst_firstname ! = TRIM(cst_firstname) 
+	prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm ! = TRIM(prd_nm) 
 
-
--- Check for unwanted Spaces for cst_lastname
+-- Check for Negative Values OR NULL 
 -- Expectation: No Result
 
-SELECT 
-	cst_lastname
-FROM silver.crm_cust_info
-WHERE cst_lastname ! = TRIM(cst_lastname)
-
-
--- Check for unwanted Spaces for gndr
--- Expectation: No Result
-
-SELECT 
-	cst_gndr
-FROM silver.crm_cust_info
-WHERE cst_gndr ! = TRIM(cst_gndr)
-
-
-SELECT 
-	cst_key
-FROM silver.crm_cust_info
-WHERE cst_key ! = TRIM(cst_key)
-
+SELECT
+	prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
 
 -- Data Standardization & Consistency
-SELECT DISTINCT cst_gndr
-FROM silver.crm_cust_info
 
-SELECT DISTINCT cst_marital_status
-FROM silver.crm_cust_info
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
 
-SELECT * FROM silver.crm_cust_info
+
+
+-- Check for Invalid Date Orders
+-- Expectation: No Result
+
+SELECT * 
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
