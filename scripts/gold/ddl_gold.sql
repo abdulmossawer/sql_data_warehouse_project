48c9b332e04b1b90ba9acbe845d847b5a434fb3c
Abdul Mossawer <142085179+abdulmossawer@users.noreply.github.com>
2025-08-08 20:05:38 +0530

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -39,3 +39,28 @@ LEFT JOIN silver.erp_cust_az12 AS ca
 ON		  ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 AS la
 ON		  ci.cst_key = la.cid
+
+
+
+
+
+CREATE VIEW gold.dim_products AS
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY prd_start_dt, prd_key) AS product_key , 
+	pn.prd_id AS product_id,
+	pn.prd_key AS product_number,
+	pn.prd_nm AS product_name,
+	pn.cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat AS subcategory,
+	pc.maintenance as maintenance,
+	pn.prd_cost AS cost,
+	pn.prd_line AS product_line,
+	pn.prd_start_dt AS product_start_date
+FROM silver.crm_prd_info AS pn
+LEFT JOIN silver.erp_px_cat_g1v2 AS pc
+ON		  pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL -- Filter Out All Historical Data
+
+
+
